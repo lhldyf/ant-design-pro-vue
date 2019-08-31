b1cb688252bd257c299c35ba6d8d24bdef4d8fb9
luohui <hui.luo@transwarp.io>
2019-08-31 21:13:00 +0800

    适应tw后端框架，JsonResult中的data

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -37,7 +37,7 @@ const user = {
     Login ({ commit }, userInfo) {
       return new Promise((resolve, reject) => {
         login(userInfo).then(response => {
-          const result = response.result
+          const result = response.data
           Vue.ls.set(ACCESS_TOKEN, result.token, 7 * 24 * 60 * 60 * 1000)
           commit('SET_TOKEN', result.token)
           resolve()
@@ -51,7 +51,7 @@ const user = {
     GetInfo ({ commit }) {
       return new Promise((resolve, reject) => {
         getInfo().then(response => {
-          const result = response.result
+          const result = response.data
 
           if (result.role && result.role.permissions.length > 0) {
             const role = result.role
