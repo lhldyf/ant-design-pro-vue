c45ee67e6af896cdc0efb64bd55c65d6688e2bde
luohui <hui.luo@transwarp.io>
2019-09-25 18:58:52 +0800

    项目可自定义logo

--- src/components/tools/Logo.vue
+++ src/components/tools/Logo.vue
@@ -1,31 +1,39 @@
 <template>
   <div class="logo">
-    <router-link :to="{path:'/'}">
-      <LogoSvg alt="logo" />
-      <h1 v-if="showTitle">{{ title }}</h1>
+    <router-link :to="{ path: '/' }">
+      <div class="logo-bg"></div>
+      <!-- <img src="~@/assets/logo-long.png" class="logo" alt="logo" /> -->
+      <!-- <LogoSvg alt="logo" /> -->
+      <!-- <h1 v-if="showTitle">{{ title }}</h1> -->
     </router-link>
   </div>
 </template>
 
 <script>
-import LogoSvg from '@/assets/logo.svg?inline'
+// import LogoSvg from '@/assets/logo.svg?inline'
 
 export default {
-  name: 'Logo',
-  components: {
-    LogoSvg
-  },
-  props: {
-    title: {
-      type: String,
-      default: 'Ant Design Pro',
-      required: false
-    },
-    showTitle: {
-      type: Boolean,
-      default: true,
-      required: false
-    }
-  }
+  name: 'Logo'
+  // components: {
+  //   LogoSvg
+  // },
+  // props: {
+  //   title: {
+  //     type: String,
+  //     default: 'Ant Design Pro',
+  //     required: false
+  //   },
+  //   showTitle: {
+  //     type: Boolean,
+  //     default: true,
+  //     required: false
+  //   }
+  // }
 }
 </script>
+
+<style lang="less" scoped>
+.sider .logo .logo-bg {
+  height: 64px;
+}
+</style>
