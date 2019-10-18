27bd0687833854814ba35b2e1da5e31ebaaae517
luohui <hui.luo@transwarp.io>
2019-10-18 10:45:03 +0800

    侧边栏的背景图标可收缩

--- src/components/tools/Logo.vue
+++ src/components/tools/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="logo">
     <router-link :to="{ path: '/' }">
-      <div class="logo-bg"></div>
+      <div class="logo-bg" v-bind:class="{'logo-bg-collapsed': collapsed}"></div>
       <!-- <img src="~@/assets/logo-long.png" class="logo" alt="logo" /> -->
       <!-- <LogoSvg alt="logo" /> -->
       <!-- <h1 v-if="showTitle">{{ title }}</h1> -->
@@ -13,27 +13,38 @@
 // import LogoSvg from '@/assets/logo.svg?inline'
 
 export default {
-  name: 'Logo'
+  name: 'Logo',
   // components: {
   //   LogoSvg
   // },
-  // props: {
-  //   title: {
-  //     type: String,
-  //     default: 'Ant Design Pro',
-  //     required: false
-  //   },
-  //   showTitle: {
-  //     type: Boolean,
-  //     default: true,
-  //     required: false
-  //   }
-  // }
+  props: {
+    title: {
+      type: String,
+      default: 'Ant Design Pro',
+      required: false
+    },
+    showTitle: {
+      type: Boolean,
+      default: true,
+      required: false
+    },
+    collapsed: {
+      type: Boolean,
+      default: false,
+      required: false
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.sider .logo .logo-bg {
+.logo .logo-bg {
+  // background: url(~@/assets/logo-long.png) left center no-repeat;
+  // width: 160px;
   height: 64px;
+}
+.logo .logo-bg.logo-bg-collapsed {
+  background: url(~@/assets/logo.png) left center no-repeat;
+  width: 64px;
 }
 </style>
