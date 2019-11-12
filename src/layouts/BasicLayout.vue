ae8539299986757095245d31d4f8e63819c32a84
luohui <hui.luo@transwarp.io>
2019-11-12 10:13:23 +0800

    还是让BasicLayout依赖global.less

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -153,6 +153,8 @@ export default {
 </script>
 
 <style lang="less">
+@import url('../components/global.less');
+
 /*
  * The following styles are auto-applied to elements with
  * transition="page-transition" when their visibility is toggled
